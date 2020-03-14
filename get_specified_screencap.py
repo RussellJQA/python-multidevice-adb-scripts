f50bfddeb0c5a184b774e7f3005b7cb43b9f56fe
Russell Johnson <16086475+RussellJQA@users.noreply.github.com>
2020-03-14 15:11:29 -0400

    Add get_specified_screencap.py to allow getting screencap for a specified device

---
+++ get_specified_screencap.py
@@ -0,0 +1,25 @@
+from get_attached_devices import get_attached_devices
+from get_screencap import adb_screencap
+
+# TODO: Parameterize this so that it can be called from the command line with a device
+#       and it will get a screenshot from that device (if it's attached
+#       [otherwise, it will give an error]).
+
+
+def get_specified_screencap(device):
+    attached_devices = get_attached_devices({f"{device}": "Nexus_6P"})
+    if attached_devices:
+        attached_device = attached_devices[device]
+        serial_no = attached_device.serial_no
+        id = attached_device.id
+        print(f"\nGetting a screenshot for {device}: (serial_no={serial_no}, id={id}).")
+        adb_screencap(serial_no=f"{serial_no}")
+
+
+def main():
+    get_specified_screencap("Huawei Nexus 6P")
+    input("\nPress any key to continue: ")
+
+
+if __name__ == "__main__":
+    main()
